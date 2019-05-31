24a3c107c470b156371bbb49039c59f4ede49e18
Tyler Reece <tyler_reece@outlook.com>
2019-05-30 22:13:43 -0400

    Added a DefaultTabController and a Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -7,94 +7,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Course',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        accentColor: Colors.amberAccent
-      ),
-      home: MyHomePage(title: 'Flutter Course Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedBottomBarIndex = 0;
-
-  void _itemTapped(int currentIndex) {
-    setState(() {
-      _selectedBottomBarIndex = currentIndex;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.call),
-            tooltip: "Call contact",
-            onPressed: () {print("calling");},
+      home: DefaultTabController(length: 3, child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(tabs: [
+            Tab(icon: Icon(Icons.home), text: "Home",),
+            Tab(icon: Icon(Icons.map), text: "Map",),
+            Tab(icon: Icon(Icons.shopping_basket), text: "Merch",),
+          ]),
+          title: Text("Flutter course"),
+        ),
+        body: TabBarView(children: [
+          Text("Welcome home"),
+          Text("Where are you? I dont know...."),
+          Text("I like that shirt too"),
+        ]),
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.alarm),
+                title: Text("Alarm"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
           ),
-          IconButton(
-            icon: Icon(Icons.add),
-            tooltip: "Add a contact",
-            onPressed: () {print("Adding a contact");},
-          )
-        ],
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the pare
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem> [
-        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
-        BottomNavigationBarItem(icon: Icon(Icons.map), title: Text("Map")),
-        BottomNavigationBarItem(icon: Icon(Icons.shop), title: Text("Merch")),
-//        BottomNavigationBarItem(icon: Icon(Icons.menu), title: Text("Menu")),
-//        BottomNavigationBarItem(icon: Icon(Icons.new_releases), title: Text("News")),
-      ],
-        currentIndex: _selectedBottomBarIndex,
-        onTap: _itemTapped,
-        fixedColor: Colors.amberAccent,
-      ),
+        ),
+      )),
     );
   }
 }
